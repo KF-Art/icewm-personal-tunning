542f923817eb6c8f491b983b7e3a311e862343d7
Kevin Figueroa <kevinfigueroart@protonmail.com>
2021-07-06 04:51:19 +0000

    Update automated_install_artix.sh

--- automated_install_artix.sh
+++ automated_install_artix.sh
@@ -15,6 +15,7 @@ sudo pacman -S --needed --noconfirm base-devel git
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
 makepkg -si
+cd ../..
 
 echo ""
 echo "Enabling Arch Linux support..."
