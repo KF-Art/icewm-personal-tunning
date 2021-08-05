6e0fb4339ba01ebad72291b6607d0c7ed49e1eab
Tuxliban <tenshalito@gmail.com>
2021-08-05 10:10:34 -0500

    Update: xed-void-install.sh
    Changes are added to detect if the directories where the configurations will be copied exist

--- scripts/xed-void-install.sh
+++ scripts/xed-void-install.sh
@@ -2,21 +2,28 @@
 
 # This script need to be run with a fully compatible POSIX shell, like sh, mksh or oksh. ZSH and Bash are not working.
 
-mkdir install
+mkdir "$HOME/github"
 echo "Installing XDEB script"
 echo ""
 
 # XDEB installation.
-sudo xbps-install -Sy binutils tar curl xz
-cd install
-git clone https://github.com/toluschr/xdeb
-cd xdeb
-chmod u+x xdeb
-mkdir ~/.local/share/bin
-sudo cp xdeb ~/.local/share/bin/
-cd ..
+sudo xbps-install -Sfy binutils bsdtar curl
+sudo ln -s /usr/bin/bsdtar /usr/local/bin/tar
+sudo ln -s /usr/bin/bsdtar /usr/local/bin/xz
 
-XDEB=~/.local/share/bin/xdeb
+git -C "$HOME/github" clone https://github.com/toluschr/xdeb
+chmod u+x "$HOME/github/xdeb/xdeb"
+if [ ! -d "$HOME/.local/share/bin" ]; then
+	mkdir "$HOME/.local/share/bin"
+	ln -s "$HOME/github/xdeb/xdeb" "$HOME/.local/share/bin/"
+else
+	ln -s "$HOME/github/xdeb/xdeb" "$HOME/.local/share/bin/"
+fi
+export PATH="$HOME/.local/share/bin:$PATH"
+
+# Temporary directory for building packages using XDEB
+mkdir /tmp/install
+INSTALL=/tmp/install
 
 echo "Installing Xed dependencies..."
 echo ""
@@ -25,20 +32,19 @@ sudo xbps-install -Sy xapps
 
 echo "Downloading Xed text editor..."
 # Download DEB packages from Linux Mint's repository.
-mkdir xed 
-cd xed
-printf "xed_2.8.4+ulyssa_amd64.deb\nxed-common_2.8.4+ulyssa_all.deb\nxed-doc_2.8.4+ulyssa_all.deb" >> xed_packages
-for i in $(cat xed_packages); do curl -O http://packages.linuxmint.com/pool/backport/x/xed/$i; done
-# Convert them with XDEB.
+cd $INSTALL
+mkdir xed && cd xed
+dash -c 'echo "xed_2.8.4+ulyssa_amd64.deb\nxed-common_2.8.4+ulyssa_all.deb\nxed-doc_2.8.4+ulyssa_all.deb"' >> xed_packages
+dash -c 'for i in $(cat xed_packages); do curl -O http://packages.linuxmint.com/pool/backport/x/xed/$i; done'
 
+# Convert them with XDEB.
 echo "Converting Xed packages with XDEB..."
 echo ""
-for i in $(cat xed_packages); do $XDEB -Sde $i; done
+dash -c 'for i in $(cat xed_packages); do xdeb -Sde $i; done'
 
 # Install converted packages.
 echo "Installing Xed text editor..."
-sudo xbps-install -y --repository binpkgs xed-2.8.4_1 xed-common-2.8.4_1 xed-doc-2.8.4_1
-cd ../..
+sudo xbps-install -y -R binpkgs xed xed-common xed-doc
 
 echo ""
 echo "Compiling Glib schemas..."
@@ -48,4 +54,7 @@ sudo glib-compile-schemas /usr/share/glib-2.0/schemas
 
 echo "Cleaning install files to save space..."
 # Clean files.
-rm -rf install
+unlink "$HOME/.local/share/bin/xdeb"
+# Delete xdeb repository if it will not be used again
+# rm -rf "$HOME/github/xdeb"
+rm -rf $INSTALL
